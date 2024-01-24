cd538dace8128b6cb08a50eb1d6a58b1994b057a
Rasul <Admin@ADMIN-PC>
2024-01-24 20:43:08 +0600

    Add project files.

---
+++ QA/BigInteger.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QA
+{
+    internal class BigInteger<T>
+    {
+        private T value; // Приватное поле для хранения чисел
+
+        public BigInteger(T initialValue)
+        {
+            this.value = initialValue; // Установка начального значения
+        }
+
+        public T Value
+        {
+            get { return value; }
+            set { this.value = value; }
+        }
+
+        public static BigInteger<T> operator +(BigInteger<T> num1, BigInteger<T> num2) // Перегрузка оператора сложения 
+        {
+            dynamic result = Convert.ToDouble(num1.Value) + Convert.ToDouble(num2.Value);
+            return new BigInteger<T>((T)Convert.ChangeType(result, typeof(T)));
+            // Реализация сложения
+        }
+
+        public static BigInteger<T> operator -(BigInteger<T> num1, BigInteger<T> num2) // Перегрузка оператора вычитания
+        {
+            dynamic result = Convert.ToDouble(num1.Value) - Convert.ToDouble(num2.Value);
+            return new BigInteger<T>((T)Convert.ChangeType(result, typeof(T)));
+            // Реализация вычитания
+        }
+
+        public static BigInteger<T> operator *(BigInteger<T> num1, BigInteger<T> num2) // Перегрузка оператора умножения
+        {
+            dynamic result = Convert.ToDouble(num1.Value) * Convert.ToDouble(num2.Value);
+            return new BigInteger<T>((T)Convert.ChangeType(result, typeof(T)));
+            // Реализация умножения
+        }
+
+        public static BigInteger<T> operator /(BigInteger<T> num1, BigInteger<T> num2) // Перегрузка оператора деления
+        {
+            if (EqualityComparer<T>.Default.Equals(num2.Value, default(T))) // Проверка деления на ноль
+            {
+                throw new ArgumentException("Деление на ноль недопустимо.");
+            }
+            dynamic result = Convert.ToDouble(num1.Value) / Convert.ToDouble(num2.Value);
+            return new BigInteger<T>((T)Convert.ChangeType(result, typeof(T)));
+            // Реализация деления
+        }
+
+    }
+}
---
+++ QA/Program.cs
@@ -0,0 +1,58 @@
+﻿using QA;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Введите первое число:");
+            decimal num1 = decimal.Parse(Console.ReadLine()); // Ввод первого числа с консоли
+
+            Console.WriteLine("Введите второе число:");
+            decimal num2 = decimal.Parse(Console.ReadLine()); // Ввод второго числа с консоли
+
+            Console.WriteLine("Выберите арифметическую операцию (+, -, *, /):");
+            char operation = Console.ReadLine()[0];
+
+            BigInteger<decimal> bigInt1 = new BigInteger<decimal>(num1); // Создание объекта BigInteger с первым числом
+            BigInteger<decimal> bigInt2 = new BigInteger<decimal>(num2); // Создание объекта BigInteger со вторым числом
+
+            BigInteger<decimal> result; // Результат операций 
+
+            switch (operation) //Выбор арифметической операции
+            {
+                case '+':
+                    result = bigInt1 + bigInt2;
+                    Console.WriteLine("Сумма: " + result.Value);
+                    break;
+                case '-':
+                    result = bigInt1 - bigInt2;
+                    Console.WriteLine("Сумма: " + result.Value);
+                    break;
+                case '*':
+                    result = bigInt1 * bigInt2;
+                    Console.WriteLine("Сумма: " + result.Value);
+                    break;
+                case '/':
+                    if (!EqualityComparer<decimal>.Default.Equals(num2, 0)) // Проверка на деление на ноль
+                    {
+                        result = bigInt1 / bigInt2; // Выполнение операции деления
+                        Console.WriteLine("Результат: " + result.Value); // Вывод результата деления
+                    }
+                    else
+                    {
+                        Console.WriteLine("Деление на ноль недопустимо."); // Вывод сообщения об ошибке при делении на ноль
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Ошибка:");
+                    break;
+            }
+        }
+        catch (Exception ex) // Обработка исключений
+        {
+            Console.WriteLine("Ошибка: " + ex.Message); // Вывод сообщения об ошибке
+        }
+    }
+}
